47c476337b6817c618cdb5ace9d2f7d166e39ebc
333Nikita333 <nikitosfox333@gmail.com>
2022-12-18 22:47:08 +0200

    finish task-02

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,25 @@
+'use strict';
+import Player from '@vimeo/player';
+const throttle = require('lodash.throttle');
 
+const iframeEl = document.querySelector('#vimeo-player');
+const player = new Player(iframeEl);
+player.on('timeupdate', throttle(onPlay, 1000));
+
+function onPlay(time) {
+  localStorage.setItem(
+    'videoplayer-current-time',
+    JSON.stringify(time.seconds)
+  );
+}
+
+const savedTime = localStorage.getItem('videoplayer-current-time');
+// Нижче код я трошки не зрозумів. Коли гуглив цей метод (setCurrentTime), стикнувся з цим.
+// Чому б просто не записати без if? Ось так: 
+// player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+
+// Як я зрозумів це для того, щоб час не зберігався у локальне сховище, якщо відео не програвалося ще. Чи це так?
+// Поясніть будь ласка)))
+if (savedTime) {
+  player.setCurrentTime(savedTime);
+}
